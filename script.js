2a4aa781812368620780e604ebd3838e7aecefb0
benlovesfoxes <97691798+benlovesfoxes@users.noreply.github.com>
2023-12-18 14:15:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
+var loggedInForCodeGenerator = false;
+var generatedCodes = [];
+var links = [
+  "https://www.algebraxyz.info/",
+  "https://www.algebraxyz.com/",
+  "https://wisdom.educational.sciencevictory.org/",
+  // Add your links here
+];
+
 function login() {
-  var password = prompt("Please enter the password:");
+  var password = document.getElementById("password").value;
   if (password === "72309") {
+    loggedInForCodeGenerator = true;
     document.getElementById("codeGeneratorSection").style.display = "block";
+    document.getElementById("password").value = "";
   } else {
-    alert("Invalid password. Please try again.");
+    alert("Incorrect password. Please try again.");
   }
 }
 
 function generateCode() {
-  var code = ""; // Add your code generation logic here
-  document.getElementById("generatedCode").innerText = "Generated Code: " + code;
+  if (loggedInForCodeGenerator) {
+    var code = Math.random().toString(36).substring(2, 8).toUpperCase();
+    generatedCodes.push({ code: code, used: false });
+    document.getElementById("generatedCode").innerHTML = "Generated Code: " + code;
+  } else {
+    alert("Please login to generate a code.");
+  }
 }
 
 function generateLink() {
   var code = document.getElementById("codeInput").value;
-  var link = "https://example.com/" + code; // Modify the link generation logic here
-  document.getElementById("generatedLink").innerHTML = "Generated Link: <a href='" + link + "'>" + link + "</a>";
+  var codeObject = generatedCodes.find(function (obj) {
+    return obj.code === code && !obj.used;
+  });
+
+  if (codeObject) {
+    var randomIndex = Math.floor(Math.random() * links.length);
+    var link = links[randomIndex] + "?code=" + code;
+    var generatedLinkElement = document.getElementById("generatedLink");
+    generatedLinkElement.innerHTML = "Generated Link: ";
+    var linkElement = document.createElement("a");
+    linkElement.href = link;
+    linkElement.textContent = link;
+    generatedLinkElement.appendChild(linkElement);
+    codeObject.used = true;
+  } else {
+    alert("Invalid code. Please try again.");
+  }
 }
 
 function logout() {
+  loggedInForCodeGenerator = false;
   document.getElementById("codeGeneratorSection").style.display = "none";
-  document.getElementById("generatedCode").innerText = "";
-  document.getElementById("generatedLink").innerText = "";
 }
