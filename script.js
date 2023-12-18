802a51e62d533377306286895ea039d966be945a
benlovesfoxes <97691798+benlovesfoxes@users.noreply.github.com>
2023-12-18 12:41:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
-document.getElementById("codeForm").addEventListener("submit", function(event) {
-  event.preventDefault();
+var loggedIn = false;
+var generatedCode = null;
+
+function login() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
   
-  var codeInput = document.getElementById("codeInput").value;
-  var links = [
-    "https://example.com/link1",
-    "https://example.com/link2",
-    "https://example.com/link3"
-  ];
+  // Perform your authentication logic here
   
-  // Validate the code
-  if (codeInput === "your-onetime-code") {
-    var randomIndex = Math.floor(Math.random() * links.length);
-    var generatedLink = links[randomIndex];
-    document.getElementById("result").innerHTML = "Generated Link: <a href='" + generatedLink + "'>" + generatedLink + "</a>";
+  if (username === "benlovesfoxes" && password === "72309") {
+    loggedIn = true;
+    document.getElementById("login-section").style.display = "none";
+    document.getElementById("code-generator-section").style.display = "block";
   } else {
-    document.getElementById("result").innerHTML = "Invalid code.";
+    alert("Invalid username or password");
   }
-});
+}
+
+function generateCode() {
+  if (loggedIn) {
+    generatedCode = generateRandomCode();
+    document.getElementById("code").innerHTML = "Generated Code: " + generatedCode;
+  } else {
+    alert("Please log in first");
+  }
+}
+
+function logout() {
+  loggedIn = false;
+  generatedCode = null;
+  document.getElementById("login-section").style.display = "block";
+  document.getElementById("code-generator-section").style.display = "none";
+  document.getElementById("username").value = "";
+  document.getElementById("password").value = "";
+}
+
+function generateRandomCode() {
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var codeLength = 6;
+  var code = "";
+  
+  for (var i = 0; i < codeLength; i++) {
+    var randomIndex = Math.floor(Math.random() * characters.length);
+    code += characters.charAt(randomIndex);
+  }
+  
+  return code;
+}
