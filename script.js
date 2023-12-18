84eb570e5e159d425f05c6d22cafff6cd7473600
benlovesfoxes <97691798+benlovesfoxes@users.noreply.github.com>
2023-12-18 13:53:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,12 @@
 var loggedInForCodeGenerator = false;
 var generatedCodes = [];
 var links = [
-  "https://www.algebraxyz.info/",
-  "https://www.algebraxyz.com/",
-  "https://wisdom.educational.sciencevictory.org/",
-  "https://www.algebraxyz.online/",
-  "https://www.math-solver.info/",
-  "https://www.recycleright.pro/",
-  "https://175.135.128.34.bc.googleusercontent.com/room",
-  "https://103.78.190.35.bc.googleusercontent.com/app",
-  "https://100.87.111.34.bc.googleusercontent.com/room",
-  "https://133.139.110.34.bc.googleusercontent.com/app",
-  "https://77.165.128.34.bc.googleusercontent.com/app",
-  "https://116.137.128.34.bc.googleusercontent.com/app",
-  "https://205.196.110.34.bc.googleusercontent.com/app",
-  "https://243.203.227.35.bc.googleusercontent.com/room",
-  "https://100.87.111.34.bc.googleusercontent.com/app",
-  "https://175.135.128.34.bc.googleusercontent.com/app",
-  "https://55gms.us/",
-  "https://55gms.pro/",
-  "https://windows11-react.vercel.app/",
-  "https://55gms.us/assets/exploits/games/yt",
-  "https://movie-web-meta-refactor.vercel.app/#/search/movie",
-  "https://shuttletv.net/",
-  "https://win11ui.netlify.app/screen",
-  "http://xn--tp9haa.glitch.me/",
-  "https://www.myturtle.net/",
-  "https://general-mathematics-beta.moaivr123.repl.co/",
-  "https://incognito-static-version.pages.dev/",
-  "https://elixir-6.hop.sh/",
-  "https://primemath.lol/",
-  "https://s-deblok.rare1k.dev/#/login",
-  "https://palladium-1.maddoxhyttinen.repl.co/",
-  "https://tsunami-1.28knowledgeherr.repl.co/",
-  "https://math-education.fowlergo.org/",
-  "https://oh-hell-nah.5inas.cf/",
-  "https://communism.95792348709187287.repl.co/",
-  "https://hypertabsstatic.brothistry.eu.org/",
-  "https://hypertabs-ex-heroku-4.nobody111.repl.co/",
-  "https://windows11.pages.dev/"
+  // Your list of links here
 ];
 
 function login() {
   var password = document.getElementById("password").value;
-  if (password === "your_password_here") {
+  if (password === "72309") {
     loggedInForCodeGenerator = true;
     document.getElementById("codeGeneratorSection").style.display = "block";
     document.getElementById("password").value = "";
@@ -53,9 +16,13 @@ function login() {
 }
 
 function generateCode() {
-  var code = Math.random().toString(36).substring(2, 8).toUpperCase();
-  generatedCodes.push(code);
-  document.getElementById("generatedCode").innerHTML = "Generated Code: " + code;
+  if (loggedInForCodeGenerator) {
+    var code = Math.random().toString(36).substring(2, 8).toUpperCase();
+    generatedCodes.push(code);
+    document.getElementById("generatedCode").innerHTML = "Generated Code: " + code;
+  } else {
+    alert("Please login to generate a code.");
+  }
 }
 
 function generateLink() {
@@ -71,4 +38,5 @@ function generateLink() {
 
 function logout() {
   loggedInForCodeGenerator = false;
-  document.getElementById("codeGeneratorSection").style
+  document.getElementById("codeGeneratorSection").style.display = "none";
+}
